6b0bf02839806d7a29bc94afa3e1a67beb40e9fd
SergeyGomanyuk <SergeyGomanyuk@yandex.ru>
2015-11-09 18:50:19 +0200

    comment with check-if-a-file-exists-with-wildcard url

--- sh/bash.sh
+++ sh/bash.sh
@@ -2,6 +2,8 @@
 
 # in case of directory use '/', i.e. '*/dir/'
 FILE_NAME_PATTERN=*.sh
+
+# http://stackoverflow.com/questions/6363441/check-if-a-file-exists-with-wildcard-in-shell-script
 echo "check if file/directory exists using name pattern $FILE_NAME_PATTERN"
 if ls -d1 $FILE_NAME_PATTERN > /dev/null 2>&1; then
 	echo "files with mask: $FILE_NAME_PATTERN exist"
